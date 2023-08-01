7c4781e7a86a8eec6c7698212d8436eb610b7b66
coinlockerbaby <seeyounextsun@outlook.com>
2023-08-01 10:01:34 +0800

    update

--- Linux_CPP/21_类与对象/7_初始化列表.cpp
+++ Linux_CPP/21_类与对象/7_初始化列表.cpp
@@ -2,30 +2,53 @@
 
 using namespace std;
 
+class boy
+{
+public:
+  string m_xm; // 姓名
+  boy ()
+  {
+    m_xm.clear ();
+    cout << "调用了boy类的构造函数" << endl;
+  }
+  boy (string xm)
+  {
+    m_xm = xm;
+    cout << "调用了boy类带参数的构造函数" << endl;
+  }
+  boy (const boy &bb)
+  {
+    m_xm = bb.m_xm;
+    cout << "调用了boy类的拷贝构造函数" << endl;
+  }
+};
+
 class girl
 {
+public:
   string m_name;
   int m_age;
+  boy m_boy;
 
-public:
-  girl ():m_name("原神"),m_age(12)  // 初始化列表
-  { cout << "调用构造函数" << endl; }
-  girl (string name, int age)
+  girl () { cout << "调用构造函数" << endl; }
+  girl (string name, int age, boy boy1)
+  :m_name(name),m_age(age),m_boy(boy1)
   {
-    m_name = name;
-    m_age = age;
+    m_boy.m_xm = boy1.m_xm;
     cout << "调用了带参数的重载构造函数" << endl;
   }
   void
   show ()
   {
-    cout << "姓名: " << m_name << "\n年龄: " << m_age << endl;
+    cout << "姓名: " << m_name << "\n年龄: " << m_age << "\n男朋友: " << m_boy.m_xm
+         << endl;
   }
 };
 
 int
 main ()
 {
-  girl girls;
+  boy boy1 ("原神");
+  girl girls ("王者", 12, boy1);
   girls.show ();
 }
